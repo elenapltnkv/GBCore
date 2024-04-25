94fab8387e15e11caee46a209b5051d82e80bad7
elenapltnkv <elenapltnkv@gmail.com>
2024-04-25 14:54:38 +0300

    4 lesson dzp2

--- src/main/java/gb4/Dz4Part1.java
+++ src/main/java/gb4/Dz4Part1.java
@@ -34,4 +34,6 @@ public class Dz4Part1 {
     }
 
 
+
+
 }
--- src/main/java/gb4/PhoneBook.java
+++ src/main/java/gb4/PhoneBook.java
@@ -2,7 +2,7 @@ package gb4;
 
 import java.util.*;
 
-public class Dz4Part2 {
+public class PhoneBook {
     /*Написать простой класс Телефонный Справочник, который хранит в себе список фамилий и телефонных номеров.
     В этот телефонный справочник с помощью метода add() можно добавлять записи, а с помощью метода get() искать номер телефона по фамилии.
 
@@ -12,27 +12,14 @@ public class Dz4Part2 {
     Желательно не добавлять лишний функционал (дополнительные поля (имя, отчество, адрес),
     взаимодействие с пользователем через консоль и т.д). Консоль использовать только для вывода результатов проверки телефонного справочника.
     */
-    HashMap<String, String> phoneNumberMap1 = new HashMap<>();
-//     phoneNumberMap1.put("79916614921", "Ivanov");
-//        phoneNumberMap1.put("79916614922", "Petrov");
-//        phoneNumberMap1.put("79916614923", "Demidov");
-//        phoneNumberMap1.put("79916614924", "Ivanov");
-//        phoneNumberMap1.put("79916614925", "Surikov");
-//        phoneNumberMap1.put("79916614926", "Petrov");
-
-
-    public static void main(String[] args) {
-
-
-    }
-
+    HashMap<String, String> phoneNumberMap = new HashMap<>();
     public void addPhones(String phone, String surname) {
-        phoneNumberMap1.put(phone, surname);
+        phoneNumberMap.put(phone, surname);
     }
 
     public List<String> getPhones(String surname) {
         ArrayList<String> phones = new ArrayList<>();
-        for (Map.Entry<String, String> entry : phoneNumberMap1.entrySet()) {
+        for (Map.Entry<String, String> entry : phoneNumberMap.entrySet()) {
             if (entry.getValue().equals(surname)) {
                 phones.add(entry.getKey());
             }
@@ -40,5 +27,16 @@ public class Dz4Part2 {
         return phones;
     }
 
+    public static void main(String[] args) {
+        PhoneBook book = new PhoneBook();
+        book.addPhones("79916614921", "Ivanov");
+        book.addPhones("79916614922", "Petrov");
+        book.addPhones("79916614923", "Demidov");
+        book.addPhones("79916614924", "Ivanov");
+        book.addPhones("79916614925", "Surikov");
+        book.addPhones("79916614926", "Petrov");
 
+        System.out.println(book.getPhones("Ivanov"));
+
+    }
 }
